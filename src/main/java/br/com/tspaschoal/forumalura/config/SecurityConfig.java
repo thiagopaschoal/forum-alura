fee08ec441a2ad495f1d6fb547a0c284265e5e67
Thiago Sakurai Paschoal <macbook@MacBook-Air-de-MacBook.local>
2021-05-04 21:15:38 -0300

    feat: add actuator

--- src/main/java/br/com/tspaschoal/forumalura/config/SecurityConfig.java
+++ src/main/java/br/com/tspaschoal/forumalura/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement()
